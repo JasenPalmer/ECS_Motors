e203bb89ee0e229806812b2a307e89aab450aabe
Jasen Palmer <palmerjase@the-royal.ecs.vuw.ac.nz>
2017-06-02 15:44:44 +1200

    connected register button with database

--- public/homepage.js
+++ public/homepage.js
@@ -2,22 +2,72 @@ $(document).ready(function(e) {
 
 	$('#login').click(function() {
 		$('#loginReq').modal();
-		$('#user').focus();
+		//$('#user').focus();
 	});
 
 	$('#submitLogin').click(function () {
 		var user = $('#user').val();
 		var pass = $('#pass').val();
 		if(user === "" || pass === ""){return;} //do some sort of error message here
-		alert("Username: "+user+" Password: "+pass);
+		//alert("Username: "+user+" Password: "+pass);
 		console.log("Username: "+user+" Password: "+pass);
-		$('#user').val("");
+		$.ajax({
+	      url: '/users',
+	      method: 'PUT',
+	      data: JSON.stringify({
+	      	username: user, 
+	      	password: pass
+	      }),
+	      dataType: 'json',
+	      contentType: "application/json; charset=utf-8",
+	      success: function(result) {
+	      	console.log("Successfully sent");
+	      },
+	      error: function(result) {
+	        console.log(result);
+	      }
+	  	});
+	  	$('#user').val("");
 		$('#pass').val("");
 	});
 
 	$('#register').click(function() {
-		alert("REGISTER");
+		$('#registerModal').modal();
 	}); 
+
+	$('#registerUser').click(function() {
+		var firstname = $('#firstname').val();
+		var lastname = $('#lastname').val();
+		var username = $('#username').val();
+		var email = $('#email').val();
+		var password = $('#password').val();
+		if(firstname === "" || lastname === "" || username === "" || email === 
+			"") {return;}
+		$.ajax({
+			url:'/users',
+			method: 'POST',
+			data: JSON.stringify({
+				'firstname': firstname,
+				'lastname': lastname,
+				'username': username,
+				'email': email,
+				'password': password
+			}),
+			dataType: 'json',
+			contentType: "application/json; charset=tf-8",
+			success: function(result) {
+	      		console.log("Successfully added new user");
+		    },
+		    error: function(result) {
+		    	console.log("Failed to add new user");
+		    }
+		});
+		$('#firstname').val("");
+		$('#lastname').val("");
+		$('#username').val("");
+		$('#email').val("");
+		$('#password').val("");
+	});
 
 	$('#srch-btn').click(function() {
 		var query = $('#srch-trm').val();
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
+
+var client = new pg.Client({
+	user: 'ldbgswgxwuvsjp',
+	password: '088cf36a1362fd7cf2b231a5d982510df58203b3a4d0b2495ca8d8fe361cccd6',
+	database: 'd2ldvtjdepq0tj',
+	port: 5432,
+	host: 'ec2-54-204-32-145.compute-1.amazonaws.com',
+	ssl: true
+});
+
+
+client.connect();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
   	title: 'ECS Motors'
    });
+});
+
+router.post('/',function(req,res) {
+
 });
 
 module.exports = router;
