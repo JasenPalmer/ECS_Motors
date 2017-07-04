7e36675be19994812a99b986a855ee03d3eb6aba
rnnrn <elliermcoyle@gmail.com>
2017-07-05 10:11:45 +1200

    :inbox_tray: static files cached for a min

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'),{maxAge: 60000}));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
